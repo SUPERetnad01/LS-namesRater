c71c5665e195833d41e6dbe3cf6a8af162fd45ab
Benno Nijholt <benno@123boeken.nl>
2022-03-09 14:02:56 +0100

    💄Red button and submit on enter

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,11 +4,13 @@
       <v-row class="justify-center">
         <v-col cols=6 md=6 sm=12>
           <h1 v-if="response" class="text-h1">{{response.message}}</h1>
-          <h2 v-if="response" class="text-h2">{{response.rating}}</h1>
+          <h2 v-if="response" class="text-h2">{{response.rating}}</h2>
           
           <v-container class="pt-12">
-            <v-text-field v-model="formData.ign" label="Summoner name" variant="contained"/>
-           <v-btn color='primary' class="justify-end" @click="getRating()">Rating!</v-btn>
+            <v-text-field v-model="formData.ign" label="Summoner name" variant="contained" @keyup.enter="getRating()"/>
+            <v-row align="center" justify="end">
+              <v-btn color='red' class="mr-4" style="color:white !important;" @click="getRating()">Rating!</v-btn>
+            </v-row>
           </v-container>
         </v-col>
       </v-row>
